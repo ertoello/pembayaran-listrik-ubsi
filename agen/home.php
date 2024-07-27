9ddab7c36c69e45f4d3159faddddb1f082b4f4dc
ertoello <ertoello17fx@gmail.com>
2024-07-27 21:20:14 +0700

    Initial commit Aplikasi Pembayaran Listrik UBSI

---
+++ agen/home.php
@@ -0,0 +1,25 @@
+<?php  
+	if (!isset($_GET['menu'])) {
+	 	header('location:hal_utama.php?menu=home');
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+</head>
+<body>
+	<div class="container-fluid">
+
+		<div class="row">
+				<img src="../images/gif.jpg" alt="gif.jpg" width="100%">
+			<!-- <div class="col-md-12">
+				<div class="alert alert-success alert-dimis">
+					<center><h4>LOGIN BERHASIL <?php echo $_SESSION['nama_agen']; ?></h4></center>
+					<h2>APLIKASI PEMBAYARAN LISTRIK PASCA BAYAR V.1.0</h2>
+				</div>
+			</div> -->
+		</div>
+	</div>
+</body>
+</html>
